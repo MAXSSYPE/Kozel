edad57d2302de68f85cdb6748f397bb4df5c93b7
Maksym <dudkamv@gmail.com>
2020-02-22 20:41:43 +0200

    grad_patch

--- app/src/main/java/com/kozel/myapplication/MenuActivity.kt
+++ app/src/main/java/com/kozel/myapplication/MenuActivity.kt
@@ -1,14 +1,19 @@
 package com.kozel.myapplication
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MenuActivity : AppCompatActivity() {
 
@@ -19,11 +24,13 @@ class MenuActivity : AppCompatActivity() {
     private lateinit var numberField2: EditText
     private lateinit var name1: TextView
     private lateinit var name2: TextView
+    private lateinit var main: LinearLayout
     private var s = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
         resultField1 = findViewById(R.id.resultField1)
         resultField2 = findViewById(R.id.resultField2)
         numberField1 = findViewById(R.id.numberField1)
@@ -31,7 +38,16 @@ class MenuActivity : AppCompatActivity() {
         name1 = findViewById(R.id.name1)
         name2 = findViewById(R.id.name2)
         raz = findViewById(R.id.textViewRaz)
+        main = findViewById(R.id.main)
         loadText(this)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        val animDrawable = main.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+
 
         numberField1.setOnEditorActionListener { textView, action, event ->
             var handled = false
@@ -92,7 +108,7 @@ class MenuActivity : AppCompatActivity() {
             raz.text = "Баллы за раздачу 1:"
             s = 1
         }
-        dialogBuilder.setNegativeButton("Нет", { dialog, whichButton -> })
+        dialogBuilder.setNegativeButton("Нет") { dialog, whichButton -> }
         val b = dialogBuilder.create()
         b.show()
     }
@@ -107,19 +123,19 @@ class MenuActivity : AppCompatActivity() {
         dialogBuilder.setPositiveButton("Да") { dialog, whichButton ->
             toSum()
         }
-        dialogBuilder.setNegativeButton("Нет", { dialog, whichButton -> })
+        dialogBuilder.setNegativeButton("Нет") { dialog, whichButton -> }
         val b = dialogBuilder.create()
         b.show()
     }
 
     private fun toSum() {
         var supp: Int
-        if (!(numberField1.text.toString() == "")) {
+        if (numberField1.text.toString() != "") {
             supp = resultField1.text.toString().toInt()
             supp += numberField1.text.toString().toInt()
             resultField1.text = supp.toString()
         }
-        if (!(numberField2.text.toString() == "")) {
+        if (numberField2.text.toString() != "") {
             supp = resultField2.text.toString().toInt()
             supp += numberField2.text.toString().toInt()
             resultField2.text = supp.toString()
@@ -139,4 +155,21 @@ class MenuActivity : AppCompatActivity() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
     }
+
+    override fun onBackPressed() {
+        saveText()
+        AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle("Выход")
+            .setMessage("Вы уверены что хотите выйти?")
+            .setPositiveButton(
+                "Да"
+            ) { _, _ ->
+                val intent = Intent(Intent.ACTION_MAIN)
+                intent.addCategory(Intent.CATEGORY_HOME)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }.setNegativeButton("Нет", null).show()
+    }
 }
+
